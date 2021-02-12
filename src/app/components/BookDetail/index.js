66dfab0484b4e151a4b5df78d12d3057464b977f
Nico <nmanograsso@gmail.com>
2021-02-12 10:46:23 -0300

    add: book detail component

---
+++ src/app/components/BookDetail/index.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {View, Text, Image, ScrollView, Pressable} from 'react-native';
+import PropTypes from 'prop-types';
+import styles from './styles';
+import bookImage from '../../../assets/img_book1.png';
+
+const BookCell = ({book}) => {
+  return (
+    <ScrollView style={styles.container}>
+      <View style={styles.cardContainer}>
+        <View style={styles.rowContainer}>
+          <Image style={styles.bookCover} source={bookImage} />
+          <View style={styles.bookInfo}>
+            <Text style={styles.title}>{book.title}</Text>
+            <Text style={styles.availability}>Available</Text>
+            <Text style={styles.author}>{book.author}</Text>
+            <Text style={styles.author}>{book.year}</Text>
+            <Text style={styles.author}>{book.genre}</Text>
+          </View>
+        </View>
+        <View style={styles.columnContainer}>
+          <Pressable style={[styles.button, styles.buttonOutline]}>
+            <Text style={[styles.textButton, styles.textBlueJeans]}>
+              ADD TO WISHLIST
+            </Text>
+          </Pressable>
+          <Pressable style={[styles.button, styles.buttonWithGradient]}>
+            <Text style={[styles.textButton, styles.textWhite]}>RENT</Text>
+          </Pressable>
+        </View>
+      </View>
+    </ScrollView>
+  );
+};
+BookCell.propTypes = {
+  book: PropTypes.shape({
+    author: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    year: PropTypes.string.isRequired,
+    genre: PropTypes.string.isRequired,
+  }),
+};
+BookCell.defaultProps = {
+  book: {title: '', author: ''},
+};
+
+export default BookCell;
---
+++ src/app/components/BookDetail/styles.js
@@ -0,0 +1,92 @@
+import {StyleSheet} from 'react-native';
+import Colors from '../../../utils/colors';
+
+export default StyleSheet.create({
+  container: {
+    backgroundColor: Colors.aliceBlue,
+  },
+  cardContainer: {
+    marginHorizontal: 16,
+    marginTop: 16,
+    paddingHorizontal: 32,
+    paddingVertical: 16,
+    height: 272,
+    borderRadius: 4,
+    flexDirection: 'column',
+    alignSelf: 'stretch',
+    justifyContent: 'space-between',
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 1.41,
+    elevation: 2,
+  },
+  bookCover: {
+    resizeMode: 'contain',
+    height: 108,
+    width: undefined,
+    aspectRatio: 1 / 1.6,
+    marginRight: 24,
+  },
+  rowContainer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    width: '100%',
+  },
+  columnContainer: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    width: '100%',
+  },
+  bookInfo: {
+    width: '72%',
+  },
+  title: {
+    color: Colors.davisGray,
+    fontWeight: 'bold',
+    fontSize: 24,
+    marginBottom: 4,
+  },
+  author: {
+    color: Colors.davisGray,
+    fontWeight: 'normal',
+    fontSize: 14,
+    marginBottom: 2,
+  },
+  availability: {
+    color: 'green',
+    fontWeight: 'bold',
+    fontSize: 14,
+    marginBottom: 2,
+  },
+  textButton: {
+    fontWeight: '900',
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  textBlueJeans: {
+    color: Colors.blueJeans,
+  },
+  textWhite: {
+    color: Colors.white,
+  },
+  button: {
+    width: '100%',
+    paddingVertical: 12,
+    borderRadius: 40,
+    marginVertical: 6,
+  },
+  buttonWithGradient: {
+    backgroundColor: Colors.blueJeans,
+  },
+  buttonOutline: {
+    color: Colors.davisGray,
+    borderColor: Colors.blueJeans,
+    borderWidth: 1.6,
+  },
+});
